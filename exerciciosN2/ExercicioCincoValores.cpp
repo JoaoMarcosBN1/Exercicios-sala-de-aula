3ef88a5ad73ad66712f17fd56efe1490dabedcaf
João Marcos Nogueira <joaomarcosb.nogueira@gmail.com>
2024-11-14 10:33:39 -0400

    v0.2

--- exerciciosN2/ExercicioCincoValores.cpp
+++ exerciciosN2/ExercicioCincoValores.cpp
@@ -13,6 +13,10 @@ for (int i = 0; i < 6; i++){
 printf("Digite um número\n");
 scanf("%d", &numero[5]);
 
-    printf("%d", numero[5]);
+        if (numero[5] < 0) {
+            printf("o número %d é negativo\n", numero[5]);
+        } else {
+            printf("o número não é negativo\n");
+        }
     }
 }
---
+++ exerciciosN2/ExercicioPrefeitura.cpp
@@ -0,0 +1,13 @@
+//João Marcos Barbosa Nogueira
+//A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de filhos. A prefeitura deseja saber:
+//a)média do salário da população;
+//b)média do número de filhos;
+//c)maior salário;
+//d)percentual de pessoas com salário até R$1900,00.
+//O final da leitura de dados se dará com a entrada de um salário negativo. (Use o comando While)
+
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+    setlocale(LC_ALL, "portuguese");
